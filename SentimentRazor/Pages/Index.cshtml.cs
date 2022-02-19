e3946c441dc66e447f110b7f31a7820c5960fb1a
MingsonZheng <MingsonZheng@outlook.com>
2022-02-19 15:48:05 +0800

    使用 ML.NET 模型生成器在 Web 应用程序中分析网站评论的情绪

---
+++ SentimentRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ML;
+
+namespace SentimentRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput> _predictionEnginePool;
+
+        public IndexModel(ILogger<IndexModel> logger, PredictionEnginePool<MLModel.ModelInput, MLModel.ModelOutput> predictionEnginePool)
+        {
+            _logger = logger;
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAnalyzeSentiment([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Neutral");
+            var input = new MLModel.ModelInput { SentimentText = text, LoggedIn = true};
+            var prediction = _predictionEnginePool.Predict(input);
+            var sentiment = Convert.ToBoolean(prediction.Prediction) ? "Toxic" : "Not Toxic";
+            return Content(sentiment);
+        }
+    }
+}
---
+++ SentimentRazor/Tool.cs
@@ -0,0 +1,14 @@
+﻿namespace SentimentRazor
+{
+    public static class Tool
+    {
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+            return fullPath;
+        }
+    }
+}
